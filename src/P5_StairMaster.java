3c94018bc5ed8397f24b64ef7a450035d82840e6
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-15 21:04:55 -0500

    Update P5_StairMaster.java

--- src/P5_StairMaster.java
+++ src/P5_StairMaster.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/15/2018
 */
 
 public class P5_StairMaster {
 
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
 
+		System.out.println("Give me the Number of stairs: ");
+		int numStairs;
+
+		numStairs = console.nextInt();
+
+		System.out.println("Give me the width");
+		int width;
+
+		width = console.nextInt();
+
+		System.out.println();
+
+		Stairs(numStairs, width);
+
+	}
+
+	// I have no clue how to go about this...
+	public static void Stairs(int numStairs, int width) {
+		for (int i = 0; i < numStairs; i++) {
+			for (int f = 0; f < width; f++) {
+				System.out.print("*");
+				System.out.println();
+				System.out.print("*");
+			}
+		}
 	}
 
 }

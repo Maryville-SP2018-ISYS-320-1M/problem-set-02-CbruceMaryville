24f674b26d8ff7846b15f49ad07f39a4064b1151
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-15 19:51:53 -0500

    Update P1_NumbersRightMethod.java

--- src/P1_NumbersRightMethod.java
+++ src/P1_NumbersRightMethod.java
@@ -1,13 +1,36 @@
-/*
-	ISYS 320
-	Name(s):
-	Date: 
-*/
 
+/*
+  	ISYS 320
+  	Name(s):Clayton Bruce
+  	Date: 4/15/2018
+*/
 public class P1_NumbersRightMethod {
 
 	public static void main(String[] args) {
-
+		drawNumbersRight(7);
+		triangleArea(8.0, 5.2, 7.1);
 	}
 
+	
+	//*****
+	//having trouble making this nested loops Im bad at nested for loops!
+	public static void drawNumbersRight(int height) {
+		
+		for (int i = 1 ; i< height+1; i ++) {
+			for(int j =1; j<height+1; j++) {
+			System.out.print(i);
+			
+			//System.out.println();
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+	System.out.println();
+	System.out.println();
+	}
 }

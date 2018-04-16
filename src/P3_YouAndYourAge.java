0d77b49eead5491e01b1a796d0898d8283bd204e
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-15 19:57:47 -0500

    Update P3_YouAndYourAge.java

--- src/P3_YouAndYourAge.java
+++ src/P3_YouAndYourAge.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/15/2018
 */
 
 public class P3_YouAndYourAge {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		
+		System.out.println("Give me your Name ");
+		String name = console.nextLine();
+		
+		System.out.println("Now what is your age? ");
+		int age = console.nextInt();
+		
+		
+		System.out.println("Wow " + name + " you've been alive "+ age +" years? Amazing.");
+		
 	}
 
 }

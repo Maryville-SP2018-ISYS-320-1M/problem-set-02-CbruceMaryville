89daffec00bcba20ea1ea90c2385c07f885b661b
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-23 19:25:49 -0500

    Update P1_NumbersRightMethod.java

--- src/P1_NumbersRightMethod.java
+++ src/P1_NumbersRightMethod.java
@@ -7,29 +7,25 @@
 public class P1_NumbersRightMethod {
 
 	public static void main(String[] args) {
-		drawNumbersRight(7);
+		drawNumbersRight(8);
 	}
 
-	
-	//*****
-	//having trouble making this nested loops Im bad at nested for loops!
+	// having trouble making this nested loops I'm bad at nested for loops!
 	public static void drawNumbersRight(int height) {
-		
-		for (int i = 1 ; i< height+1; i ++) {
-			for(int j =1; j<height+1; j++) {
-			System.out.print(i);
-			
-			//System.out.println();
+
+		for (int row = 1; row < height + 1; row++) {
+			for (int spaces = 1; spaces <= height - row; spaces++) {
+				System.out.print(" ");
 			}
+			for (int Column = 1; Column <= row; Column++) {
+				System.out.print(row);
+
 			
-			
-			
+			}
+			System.out.println();
 		}
-		
-		
-		
-		
-	System.out.println();
-	System.out.println();
+
+		System.out.println();
+		System.out.println();
 	}
 }

576451fd9573245fe031f0c0376a8c620788faf0
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-23 20:04:12 -0500

    Update P5_StairMaster.java

--- src/P5_StairMaster.java
+++ src/P5_StairMaster.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 	ISYS 320
 	Name(s):Clayton Bruce
 	Date: 4/15/2018
+	Edited on : 4/23
+	Still confused
 */
 
 public class P5_StairMaster {
@@ -11,30 +13,58 @@ public class P5_StairMaster {
 		Scanner console = new Scanner(System.in);
 
 		System.out.println("Give me the Number of stairs: ");
-		int numStairs;
+		int height;
 
-		numStairs = console.nextInt();
+		height = console.nextInt();
 
-		System.out.println("Give me the width");
+		System.out.println("Give me the width of each stair : ");
 		int width;
 
 		width = console.nextInt();
+		
+		//Confused on how to get the number of spacers
+		System.out.println("Give me the number of spacers: ");
+		int NumOfSpacers;
+		
+		NumOfSpacers = console.nextInt();
 
-		System.out.println();
-
-		Stairs(numStairs, width);
+	
+		
+		for(int row= 0; row <height; row++) {
+			PrintWall(height);
+			PrintSpace(NumOfSpacers, width);
+			PrintStep(width);
+			System.out.println();
+			
+		}
 
 	}
 
-	// I have no clue how to go about this...
-	public static void Stairs(int numStairs, int width) {
-		for (int i = 0; i < numStairs; i++) {
-			for (int f = 0; f < width; f++) {
-				System.out.print("*");
-				System.out.println();
-				System.out.print("*");
+	public static void PrintStep(int width) {
+		for (int i = 0; i < width; i++) {
+			System.out.print("*");
+			System.out.print("*");
+		}
+
+	}
+
+	public static void PrintSpace(int NumOfSpacers, int width) {
+		for (int x = 0; x < NumOfSpacers; x++) {
+			for (int i = 0; i < width; i++) {
+				System.out.print(" ");
+				System.out.print(" ");
 			}
 		}
+
 	}
+	//changing wall to || so see where they are showing up
+	public static void PrintWall(int numStair) {
+		for (int i = 0; i <= numStair; i++) {
+			System.out.print("||");
+			//System.out.print("||");
+		}
+	}
+
+	
 
 }

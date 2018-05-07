bcca14e94e22dcc23cc52a217788f78cc2c4e7c7
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-05-06 20:25:50 -0500

    Update P2_AreaComputer.java

--- src/P2_AreaComputer.java
+++ src/P2_AreaComputer.java
@@ -2,7 +2,6 @@
 	ISYS 320
   	Name(s):Clayton Bruce
   	Date: 4/15/2018
-	FIXED on 5/6/2018
 */
 
 public class P2_AreaComputer {
@@ -10,14 +9,14 @@ public class P2_AreaComputer {
 	public static void main(String[] args) {
 		System.out.println(triangleArea(8.0, 5.2, 7.1));
 	}
-	
-	
-	//Triangle area function
-	    public static double triangleArea(double len1, double len2, double len3){ 
-	    	double area; double s;
-	        s = 0.5 * (len1 + len2 + len3);
-	        area = Math.sqrt(s*(s-len1)*(s-len2)*(s-len3));
-	        System.out.println("Area of triangle is: ");
-	        return area;
-	    }
+
+	// Triangle area function
+	public static double triangleArea(double len1, double len2, double len3) {
+		double area;
+		double s;
+		s = 0.5 * (len1 + len2 + len3);
+		area = Math.sqrt(s * (s - len1) * (s - len2) * (s - len3));
+		System.out.println("Area of triangle is: ");
+		return area;
+	}
 }
